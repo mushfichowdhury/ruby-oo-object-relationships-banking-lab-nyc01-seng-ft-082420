c621aaad44e31a8054e0f1e11e40300463c250f4
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-08-28 11:26:24 -0400

    Complete

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,41 @@
 class BankAccount
+    attr_reader :name 
+    attr_accessor :balance, :status
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end 
+
+    def deposit(amount)
+        @balance = @balance + amount.to_i
+    end 
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid? 
+        if self.status == "open" && self.balance > 0
+            true
+        else 
+            false
+        end
+    end
+
+    def close_account
+        if self.status == "open"
+            self.status = "closed"
+        else
+            "You don't have an exisitng account."
+        end 
+    end 
 
 end
